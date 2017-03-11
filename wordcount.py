a68f4aaf640bb0de444a7b2dbf1a59ad9be95f76
Kuan Yu Chen <chenky0916@gmail.com>
2017-03-11 21:53:42 +0800

    modified wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,7 +1,7 @@
 from collections import Counter
 import nltk
-nltk.download('stopwords')
-nltk.download('averaged_perceptron_tagger')
+#nltk.download('stopwords')
+#nltk.download('averaged_perceptron_tagger')
 from nltk.corpus import stopwords
 stopwords = set(stopwords.words('english'))
 
